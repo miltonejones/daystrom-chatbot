86fd1daa0823003711096f21b528bc9e5e05997e
Milton Jones <miltonejones@gmail.com>
2023-09-14 13:12:49 -0500

    added security

--- src/machines/actions/chatActions.js
+++ src/machines/actions/chatActions.js
@@ -180,7 +180,8 @@ export const chatActions = {
   assignUserList: assign((_, event) => {
     const encryptedCredentials = event.data;
     const credentials = decryptCredentials(event.data);
-    console.log(JSON.stringify({ credentials, encryptedCredentials }, 0, 2));
+    console.log({ encryptedCredentials, credentials });
+    // console.log(JSON.stringify({ credentials, encryptedCredentials }, 0, 2));
     return {
       credentials,
       encryptedCredentials,
